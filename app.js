281194d016834cfa1712297f08c5ef71efefb9bd
Nazaryan543 <111902735+Nazaryan543@users.noreply.github.com>
2022-10-04 12:57:53 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
-const server = http.createServer(function(req, res){
+const server = http.createServer(function(req, res,min,max,){
+    min = 1;
+    max = 8;
+    let S=0;
+    for(i=min; i<=max; i++){
+      res.write(i.toString()+"+")
+       S=S+i}
+    
+      res.write("="+ S.toString())
+      res.end()
 
-for(i=1; i<=10;i=i+1){
-    res.write(i.toString())}
-res.end()
 
 })
 server.listen(3000)
